c0aa40eac98f5c21b6fc85c817777f4153dcdcd8
rubik <rubixgeek795@gmail.com>
2022-08-27 20:32:15 +0800

    feat: init

---
+++ src/pages/game/components/square/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Square() {
+    return (
+        <>
+            <div className="square">
+                <button>
+                    {/* {TODO} */}
+                </button>
+            </div>
+        </>
+    )
+}
---
+++ src/pages/game/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Board from './components/board'
+
+export default function Game(props) {
+    return (
+        <div>
+            <h1>hello world</h1>
+            {/* <Board /> */}
+        </div>
+    )
+}
